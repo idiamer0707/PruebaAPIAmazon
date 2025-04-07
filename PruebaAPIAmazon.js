119004a88c6fc9297842b83b759a2f7e39a881f4
idiamer0707 <157150292+idiamer0707@users.noreply.github.com>
2025-04-07 14:27:35 +0200

    Update PruebaAPIAmazon.js

--- PruebaAPIAmazon.js
+++ PruebaAPIAmazon.js
@@ -1,8 +1,11 @@
-const url = "https://api.musicapi.com/api/123e4567-e89b-12d3-a456-426614174000/artists/987654321";
 
-fetch(url, requestOptions)
-    .then((response) => response.json()) // Cambia .text() a .json() si el resultado es JSON
-    .then((result) => console.log('Información del artista:', result))
-    .catch((error) => console.error('Error:', error));
+document.getElementById('login').addEventListener('click', () => {
+    const url = "https://api.musicapi.com/api/123e4567-e89b-12d3-a456-426614174000/artists/987654321";
+
+    fetch(url, requestOptions)
+        .then((response) => response.json()) // Cambia .text() a .json() si el resultado es JSON
+        .then((result) => console.log('Información del artista:', result))
+        .catch((error) => console.error('Error:', error));
+});
 
 
